6e0b8706ab848f47c82fe8a47d7f1c532214b0e8
raiqi1 <ramadifan769@gmail.com>
2025-09-13 14:00:22 +0700

    add readme

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "ai-location-assistant", // bebas, nama aplikasinya
+      name: "ai-location-assistant",
       script: "uvicorn",
       args: "main:app --host 0.0.0.0 --port 8110",
       interpreter: "python3",
-      cwd: "/root/test", // path project kamu
+      cwd: "/root/test",
       env: {
         ENVIRONMENT: "production",
       },
